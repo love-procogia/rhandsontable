2858b32de0241ec53607cae197fc2f2fbe98a93e
timelyportfolio <kent.russell@timelyportfolio.com>
2022-09-17 09:57:16 -0500

    register Swiss with `numbro`

--- inst/htmlwidgets/lib/numbro/languages.js
+++ inst/htmlwidgets/lib/numbro/languages.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var language = {
+    var language_german = {
       languageTag: "de-DE",
       delimiters: {
           thousands: ".",
@@ -51,12 +51,62 @@
       }
     }
 
-    // Node
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = language;
+    var language_swiss = {
+      languageTag: "de-CH",
+      delimiters: {
+          thousands: "’",
+          decimal: "."
+      },
+      abbreviations: {
+          thousand: "k",
+          million: "m",
+          billion: "b",
+          trillion: "t"
+      },
+      ordinal: function() {
+          return ".";
+      },
+      currency: {
+          symbol: "CHF",
+          position: "postfix",
+          code: "CHF"
+      },
+      currencyFormat: {
+          thousandSeparated: true,
+          totalLength: 4,
+          spaceSeparated: true,
+          average: true
+      },
+      formats: {
+          fourDigits: {
+              totalLength: 4,
+              spaceSeparated: true,
+              average: true
+          },
+          fullWithTwoDecimals: {
+              output: "currency",
+              mantissa: 2,
+              spaceSeparated: true,
+              thousandSeparated: true
+          },
+          fullWithTwoDecimalsNoCurrency: {
+              mantissa: 2,
+              thousandSeparated: true
+          },
+          fullWithNoDecimals: {
+              output: "currency",
+              spaceSeparated: true,
+              thousandSeparated: true,
+              mantissa: 0
+          }
+      }
     }
+
     // Browser
     if (typeof window !== 'undefined' && window.numbro && window.numbro.registerLanguage) {
-        this.numbro.registerLanguage(language);
+        this.numbro.registerLanguage(language_german);
+        this.numbro.registerLanguage(language_swiss);
     }
 }.call(typeof window === 'undefined' ? this : window));
+
+
